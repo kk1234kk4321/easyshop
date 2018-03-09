1c30e66a4b88090e507db7801c94a75c07357273
kk1234kk4321 <1503855424@qq.com>
2018-03-09 16:29:44 +0800

    websocket

--- app.js
+++ app.js
@@ -3,26 +3,86 @@ App({
     console.log('App Launch', options);
     console.log('getSystemInfoSync', my.getSystemInfoSync());
     console.log('SDKVersion', my.SDKVersion);
+
+
+    my.connectSocket({
+      url: 'ws://localhost:8080/WebsocketHome/actions', // 开发者服务器接口地址，必须是 wss 协议，且域名必须是后台配置的合法域名
+    })
+
+    my.onSocketClose((res) => {
+      my.alert({ content: '服务器通信异常！' });
+    });
+
+    my.onSocketOpen((res) => {
+      console.log("连接已经打开");
+    });
+
+    my.onSocketError(function (res) {
+      my.alert({content:'服务器连接失败！' + res});
+    });
+
+    my.onSocketMessage((res) => {
+      var data = JSON.parse(res.data);
+      if (data.cmd == undefined) {
+        return;
+        my.alert({ content: '收到数据！' + res.data });
+
+      }
+      switch (data.cmd) {
+        case 100://免密授权
+          my.paySignCenter({
+            signStr: data.signStr,
+            success: (res) => {
+
+            },
+            fail: (res) => {
+              
+            }
+          });
+          break;
+        case 2:
+
+          break;
+        default:
+
+      }
+    });
+
+
+    my.getAuthCode({
+      scopes: 'auth_base', // 主动授权（弹框）：auth_user，静默授权（不弹框）：auth_base
+      success: (res) => {
+        var data = {
+          "auth_code": res.authCode,
+          "deviceId": this.globalData.deviceId,
+          "boxId": this.globalData.boxId,
+          "cmd": 100 //扫码进入小程序，传递auth_code
+        }
+        this.globalData.authCode =  res.authCode;
+        my.sendSocketMessage({
+          data: JSON.stringify(data)
+        });
+      },
+    });
   },
+
   onShow() {
     console.log('App Show');
   },
   onHide() {
     console.log('App Hide');
   },
+
   globalData: {
-    hasLogin: false,
+    authCode: "",
+    deviceId:1000086,
+    boxId:1
   },
   userInfo: null,
+
   getUserInfo() {
     return new Promise((resolve, reject) => {
       if (this.userInfo) resolve(this.userInfo);
-
-      my.getAuthCode({
-        scopes: ['auth_user'],
-        success: (authcode) => {
-          console.info(authcode);
-
           my.getAuthUserInfo({
             success: (res) => {
               this.userInfo = res;
@@ -32,11 +92,6 @@ App({
               reject({});
             },
           });
-        },
-        fail: () => {
-          reject({});
-        },
-      });
     });
   },
 
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,11 @@
+const app = getApp();
 
 Page({
+  
+  onLoad() {
+
+
+  },
 
   toMyinfo(e){
       my.navigateTo({
@@ -17,7 +23,25 @@ Page({
       my.navigateTo({
       url: `/pages/add-replenish/add-replenish`,
     });
+  },
+
+    toWebSocket(e){
+      var date ={
+        "authcode": app.globalData.authCode,
+        "timestamp": Date.parse(new Date()),
+        "keepTime": "3600",
+        "deviceId":app.globalData.deviceId,
+        "boxId":app.globalData.boxId,
+        "cmd":200,//购物开门
+      }; 
+      var str = JSON.stringify(date); 
+      my.sendSocketMessage({
+      data: str
+      });
+
   }
+
+
 
 
 
