ebf101d0e924086065049a95e4fd5ed01bb59f14
hanbing5210 <2487825637@qq.com>
2018-11-03 19:50:15 +0800

    新增，用户识别号(我的编号)复制

--- pages/myinfo/myinfo.js
+++ pages/myinfo/myinfo.js
@@ -7,12 +7,14 @@ Page({
   onLoad() {   
     this.setData({
       userId:app.globalData.userId
+      // userId:'1231546113565123'
     });
     app.getUserInfo().then(
       user => this.setData({
         user,
       }),
     );
+    
   },
 
   toOrder(e){
@@ -20,4 +22,19 @@ Page({
       url: `/pages/add-order/add-order`,
     });
   },
+  copyCode(e){
+    my.setClipboard({
+      text: this.data.userId, // 剪贴板数据
+      success: (res) => {
+        my.getClipboard({
+          success: (res) => {
+            my.alert({
+              title: '复制成功' 
+            });
+          console.log("userid",res.text)
+          },
+        });
+      },
+    });
+  }
 });
