34c8898cdb73bee5f5b83b351289684857c3f524
kk1234kk4321 <1503855424@qq.com>
2018-12-03 16:19:47 +0800

    聚合码

--- app.js
+++ app.js
@@ -1,25 +1,43 @@
 App({
-  
+
   onLaunch(options) {//2017120700423854
     //my.alert({content: '启动参数：'+JSON.stringify(options.query)});
     console.log('启动参数:App Launch', options);
-     if(options.query){
-        if(options.query.deviceId){
-        this.globalData.deviceId = options.query.deviceId;
+
+    if (options.query) {
+      var link = options.query.qrCode;
+      console.log("参数link", link);
+      if (link) {
+        var regex = /\/deviceId\/(\S*)\//;
+        var result;
+        if ((result = regex.exec(link)) != null) {
+          this.globalData.deviceId = result[1];
         }
-        if(options.query.openType){
+        regex = /\/openType\/(\S*)\//;
+        if ((result = regex.exec(link)) != null) {
+          this.globalData.openType = result[1];
+        }
+
+      } else {
+        if (options.query.deviceId) {
+          this.globalData.deviceId = options.query.deviceId;
+        }
+        if (options.query.openType) {
           this.globalData.openType = options.query.openType;
         }
-        console.log("参数deviceId",this.globalData.deviceId);
-        console.log("参数openType",this.globalData.openType);
-     }
+      }
+ 
+    }
+
+    console.log("参数deviceId", this.globalData.deviceId);
+    console.log("参数openType", this.globalData.openType);
     console.log('getSystemInfoSync', my.getSystemInfoSync());
     console.log('SDKVersion', my.SDKVersion);
 
     my.connectSocket({
       //url: 'ws://localhost:8080/WebsocketHome/actions', // 开发者服务器接口地址，必须是 wss 协议，且域名必须是后台配置的合法域名
       //url: 'ws://222.186.101.234:8090/erpLife/socket/websocket'
-      url: 'ws://117.89.14.55:8088/erpLife/socket/websocket'
+      url: 'ws://erp.zhangyuanzhineng.com:8080/erpLife/socket/websocket'
       //url: 'ws://192.168.1.115:8080/erpLife/socket/websocket'
     })
 
@@ -32,34 +50,34 @@ App({
     });
 
     my.onSocketOpen((res) => {
-      console.log("连接已经打开",res);
+      console.log("连接已经打开", res);
 
       my.getAuthCode({
-      scopes: 'auth_base',
-      success: (res) => {
-        let msg = {
-          "authcode": res.authCode,
-          "deviceId": this.globalData.deviceId,
-          "boxId": this.globalData.boxId,
-          "cmd": 100 //扫码进入小程序，传递auth_code
-        }
-        this.globalData.authCode = res.authCode;
-        console.log("发送cmd100|扫码",msg);
-        my.sendSocketMessage({
-          data: JSON.stringify(msg),
-          success: (res) => {
-              console.log("send cmd100 success");
-          },
-          fail:(res) => {
-            console.log("send cmd100 fail",res);
+        scopes: 'auth_base',
+        success: (res) => {
+          let msg = {
+            "authcode": res.authCode,
+            "deviceId": this.globalData.deviceId,
+            "boxId": this.globalData.boxId,
+            "cmd": 100 //扫码进入小程序，传递auth_code
           }
-        });
-      },
-    });
-    
+          this.globalData.authCode = res.authCode;
+          console.log("发送cmd100|扫码", msg);
+          my.sendSocketMessage({
+            data: JSON.stringify(msg),
+            success: (res) => {
+              console.log("send cmd100 success");
+            },
+            fail: (res) => {
+              console.log("send cmd100 fail", res);
+            }
+          });
+        },
+      });
+
     });
 
-    my.onSocketError(function (res) {
+    my.onSocketError(function(res) {
       my.alert({ content: '服务器连接失败！' + res });
     });
 
@@ -70,16 +88,16 @@ App({
 
   globalData: {
     authCode: "",
-    deviceId:"100200100001",//99872212,
-    boxId:1,
-    userId:"",//"2088902710839148",//"2088112422848101",
-    userType:0,
-    openType:'',//"palm"
-    erpUrl: 'http://117.89.14.55:8088/'
+    deviceId: "",//"100200100001",//99872212,
+    boxId: 1,
+    userId: "",//"2088902710839148",//"2088112422848101",
+    userType: 0,
+    openType: '',//"palm"
+    erpUrl: 'https://erp.zhangyuanzhineng.com/'
   },
   userInfo: null,
 
- getUserInfo() {
+  getUserInfo() {
     return new Promise((resolve, reject) => {
       if (this.userInfo) resolve(this.userInfo);
       // 调用用户授权 api 获取用户信息
@@ -92,23 +110,23 @@ App({
             // scopes: ['auth_base'],
             success: (res) => {
               this.userInfo = res;
-              console.log("userinf",this.userInfo)
+              console.log("userinf", this.userInfo)
               resolve(this.userInfo);
             },
-            fail: () => { 
+            fail: () => {
               console.log("获取信息失败")
               reject({});
             },
           });
         },
-        fail: () => { 
+        fail: () => {
           reject({});
         },
       });
     });
   },
 
- 
+
 
 
 
